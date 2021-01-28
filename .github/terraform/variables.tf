2a3590e5165f311dcfde8168f790c08dd1ba05ae
Johanes Alexander <alexander.v21@outlook.com>
2021-01-28 11:52:19 +0700

    add-iac

---
+++ .github/terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+}
+
+variable "resource_group_location" {
+  description = "The location of the resource group"
+}
+
+variable "aks_name" {
+  description = "The name of the kubernetes"
+}
+
+variable "client_id" {
+  description = "The name of the kubernetes"
+}
+
+variable "client_secret" {
+  description = "The name of the kubernetes"
+}
